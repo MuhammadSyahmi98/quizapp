5ff50c9253684b03d29a803f4544520fc49b9af6
Muhammad Syahmi <syahmijalil12@gmail.com>
2021-07-16 22:33:46 +0800

    Admin authetication middleware

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $admin = new User();
+        $admin->name="admin";
+        $admin->email="admin@gmail.com";
+        $admin->password = bcrypt('password');
+        $admin->visible_password = "password";
+        $admin->email_verified_at = NOW();
+        $admin->occupation="CEO";
+        $admin->address = "Address here";
+        $admin->phone="011111121342";
+        $admin->is_admin = 1;
+        $admin->save();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,62 +16,71 @@ use App\Http\Controllers\QuestionController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin/index');
-});
 
-Auth::routes();
+
+Auth::routes([
+    'register'=>false,
+    'reset'=>false,
+    'verify'=>false,
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-// Here is quiz
+Route::group(['middleware'=>'isAdmin'], function(){
+    Route::get('/', function () {
+        return view('admin/index');
+    });
+    // Here is quiz
 
-Route::get('/quiz/create', [QuizController::class, 'create'])->name('quiz-create');
-Route::post('/quiz/create', [QuizController::class, 'store'])->name('quiz-store');
-Route::get('/quiz', [QuizController::class, 'index'])->name('quiz-index');
-Route::delete('/quiz/{id}', [QuizController::class, 'destroy'])->name('quiz-delete');
-Route::get('/quiz/edit/{id}', [QuizController::class, 'edit'])->name('quiz-edit');
-Route::put('/quiz/edit/{id}', [QuizController::class, 'update'])->name('quiz-update');
+    Route::get('/quiz/create', [QuizController::class, 'create'])->name('quiz-create');
+    Route::post('/quiz/create', [QuizController::class, 'store'])->name('quiz-store');
+    Route::get('/quiz', [QuizController::class, 'index'])->name('quiz-index');
+    Route::delete('/quiz/{id}', [QuizController::class, 'destroy'])->name('quiz-delete');
+    Route::get('/quiz/edit/{id}', [QuizController::class, 'edit'])->name('quiz-edit');
+    Route::put('/quiz/edit/{id}', [QuizController::class, 'update'])->name('quiz-update');
 
-Route::get('/quiz/question/{id}', [QuizController::class, 'question'])->name('quiz-question');
+    Route::get('/quiz/question/{id}', [QuizController::class, 'question'])->name('quiz-question');
 
-// End quiz
+    // End quiz
 
-// =================================================================================================
+    // =================================================================================================
 
-// Here is question
+    // Here is question
 
-Route::get('/question/create', [QuestionController::class, 'create'])->name('question-create');
-Route::get('/question', [QuestionController::class, 'index'])->name('question-index');
+    Route::get('/question/create', [QuestionController::class, 'create'])->name('question-create');
+    Route::get('/question', [QuestionController::class, 'index'])->name('question-index');
 
-Route::delete('/question/{id}', [QuestionController::class, 'destroy'])->name('question-delete');
+    Route::delete('/question/{id}', [QuestionController::class, 'destroy'])->name('question-delete');
 
-Route::get('/question/show/{id}', [QuestionController::class, 'show'])->name('question-show');
+    Route::get('/question/show/{id}', [QuestionController::class, 'show'])->name('question-show');
 
-Route::get('/question/edit/{id}', [QuestionController::class, 'edit'])->name('question-edit');
-Route::put('/question/edit/{id}', [QuestionController::class, 'update'])->name('question-update');
+    Route::get('/question/edit/{id}', [QuestionController::class, 'edit'])->name('question-edit');
+    Route::put('/question/edit/{id}', [QuestionController::class, 'update'])->name('question-update');
 
-Route::post('/question/create', [QuestionController::class, 'store'])->name('question-store');
+    Route::post('/question/create', [QuestionController::class, 'store'])->name('question-store');
 
-// End question
+    // End question
 
-// =================================================================================================
-
-
-// =================================================================================================
-
-// Here is User
-
-Route::get('/user', [UserController::class, 'index'])->name('user-index');
-Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user-destroy');
-Route::get('/user/create', [UserController::class, 'create'])->name('user-create');
-Route::post('/user/create', [UserController::class, 'store'])->name('user-store');
-
-Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user-edit');
-Route::put('/user/edit/{id}', [UserController::class, 'update'])->name('user-update');
+    // =================================================================================================
 
 
-// End User
+    // =================================================================================================
 
-// =================================================================================================
+    // Here is User
+
+    Route::get('/user', [UserController::class, 'index'])->name('user-index');
+    Route::delete('/user/{id}', [UserController::class, 'destroy'])->name('user-destroy');
+    Route::get('/user/create', [UserController::class, 'create'])->name('user-create');
+    Route::post('/user/create', [UserController::class, 'store'])->name('user-store');
+
+    Route::get('/user/edit/{id}', [UserController::class, 'edit'])->name('user-edit');
+    Route::put('/user/edit/{id}', [UserController::class, 'update'])->name('user-update');
+
+
+    // End User
+
+    // =================================================================================================
+
+});
+
